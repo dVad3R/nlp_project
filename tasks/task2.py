7d6f25e71181f4d98570ba34e9fbc4f710c8fb45
DimosKap <dimostheniskap13@gmail.com>
2021-07-28 13:03:44 +0300

    Optimized task2 and editted mary.txt

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,6 +1,7 @@
 # pip install --user -U nltk
 
 import nltk
+from nltk import text
 import spacy
 
 grammar = nltk.CFG.fromstring(""" 
@@ -9,7 +10,7 @@ NP -> PN | Det N | N
 VP -> IV | IV ADV | AV ADJ | TV PN NP | V NP 
 Det -> 'the' | 'a' | 'an' 
 ADJ -> 'scary' | 'tall' | 'short' | 'blonde' | 'slim' | 'fat' 
-N -> 'cat' | 'dog' | 'cats' | 'food'
+N -> 'cat' | 'dog' | 'cats' | 'food' | 'book'| 'books'
 AV -> 'is' | 'does' | 'are' | 'do' 
 IV -> 'runs' | 'run' | 'running' | 'hurts' | 'hurt' | 'hurting' | 'walks' | 'walk' | 'walking' | 'jumps' | 'jump' | 'jumping' | 'shoots' | 'shoot' | 'shooting' 
 TV -> 'gives' | 'give' | 'gave' | 'giving' 
@@ -26,13 +27,28 @@ mary_txt = open("../resources/mary.txt", "r").read()
 # load English tokenizer, tagger, parser and NER
 nlp = spacy.load("en_core_web_sm")
 
-# parse story into the nlp
-story1_processed = nlp(dog_txt)
-story2_processed = nlp(mary_txt)
 
 parser = nltk.ChartParser(grammar) 
 
-for sentence in story1_processed.sents:
+
+
+text_file = ""
+
+try: 
+    text_selection = int(input("[*] Please select a text file (1 or 2).\n"))
+
+    if text_selection == 1:
+        text_file = dog_txt
+    elif text_selection == 2:
+        text_file = mary_txt
+    else:
+        print("Invalid input. Selecting default text file...")
+        text_file = dog_txt
+except:
+    print("Invalid input. Selecting default text file.")
+    text_file = dog_txt
+
+for sentence in nlp(text_file).sents:
 
     # print current sentence
     sent = sentence.text.replace(".","").split(" ")
